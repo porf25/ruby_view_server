202f4f8e5bbe865a5ca271c06ab32905e15fe1d9
Porfirio Bonet <porfirio.bonet@gmail.com>
2015-03-24 11:57:28 -0400

    homework section 1 completed

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,20 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+name = "porf"
+final_string = "#{name} is the man that is going to learn ruby nice"
+output_string = ERB.new "<%= final_string %>"
+puts output_string.result(binding)
+
+time_out = ERB.new "<%= Time.now.hour %>"
+puts time_out.result(binding)
+
+animal = ["porf", "kirk", "willy"]
+animal_out = ERB.new "<%= animal %>"
+puts animal_out.result(binding)
+
+a_out = ERB.new '<% animal.each do |p| %>
+				  <%= puts "killa #{p}"%>
+				 <% end %>'
+puts a_out.result(binding)				 
